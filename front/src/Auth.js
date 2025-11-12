6e885d7c853dad2f528b58a241f0c3690d1a97b2
Sri <159001650+Srinu2307@users.noreply.github.com>
2025-11-12 23:02:43 +0530

    Update Auth.js

--- front/src/Auth.js
+++ front/src/Auth.js
@@ -16,7 +16,7 @@ export default function AuthPage() {
   const handleRegister = async(e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://my-blog-app-backend-c3it.onrender.com/api/auth/register", {
+      const response = await fetch("https://my-blog-app-backend-1-8j1a.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -35,7 +35,7 @@ export default function AuthPage() {
   const handleLogin = async(e) =>{
     e.preventDefault();
     try{
-          const response = await fetch('https://my-blog-app-backend-c3it.onrender.com/api/auth/login',{
+          const response = await fetch('https://my-blog-app-backend-1-8j1a.onrender.com/api/auth/login',{
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email: loginEmail, password: loginPassword}) 
